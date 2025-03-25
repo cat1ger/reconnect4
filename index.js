894addab74fc0b5d783f5de0d1687f72eb5d04c5
TigerJC <164162007+cat1ger@users.noreply.github.com>
2025-03-25 10:44:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -691,7 +691,7 @@ function createExportWrapper(name, nargs) {
 var wasmBinaryFile;
 
 function findWasmBinary() {
-    return locateFile('index.wasm');
+    return locateFile('connect4.wasm');
 }
 
 function getBinarySync(file) {
